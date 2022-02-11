b4e2ca5bd3da81a049f9be39885986e0bb5a7756
viggo-jacobsson-nti-johanneberg <viggo.jacobsson@elev.ga.ntig.se>
2022-02-11 14:31:37 +0100

    Skapade första layout med färger, kommandon för hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Denna fil skall innehålla JavaScript (JS)
+ 
+const nav = document.querySelector('.navlinks')
+const burger = document.querySelector('.burger')
+ 
+nav.classList.toggle('navActive')
+ 
+function toggleMeny() {
+ 
+   nav.classList.toggle('navActive')
+   burger.classList.toggle('toggle')
+}
+ 
+ 
+burger.addEventListener('click', toggleMeny)
+nav.addEventListener('click', toggleMeny)
+ 
+ 
