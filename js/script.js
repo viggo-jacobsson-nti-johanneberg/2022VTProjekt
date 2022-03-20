feeefe19de9e4bc4331db4407aa900e04e3e6e08
viggo-jacobsson-nti-johanneberg <viggo.jacobsson@elev.ga.ntig.se>
2022-03-20 13:41:53 +0100

    "Result" sida, sök-funktion, scroll-to-top-funktion på index

--- js/script.js
+++ js/script.js
@@ -14,4 +14,9 @@ function toggleMeny() {
 burger.addEventListener('click', toggleMeny)
 nav.addEventListener('click', toggleMeny)
  
- 
+
+ // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
