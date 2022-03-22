5360d1efb67f6b61185a4874bc5b0b91ce98925c
viggo-jacobsson-nti-johanneberg <viggo.jacobsson@elev.ga.ntig.se>
2022-03-22 14:59:28 +0100

    Relatedbox, colorswap, productpages
    Related box - "Relatedbox" skapad och gjort för varje produktdelsida
    
    Colorswap - Scroll-menu för att byta färger fixad
    
    Produktdelsidor - Skapat delsidor för varje produktfärg

--- js/script.js
+++ js/script.js
@@ -20,3 +20,29 @@ function topFunction() {
    document.body.scrollTop = 0; // For Safari
    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function togglecarted() {
+   document.getElementById("cartIcon").src = "../img/added_to_cart.svg"
+}
+
+
+
+function togglebrown () {
+   document.getElementById("productimg").src = "../img/BrownGuitar.jpg"
+}
+function toggleblue () {
+   document.getElementById("productimg").src = "../img/BlueGuitar.jpg"
+}
+function togglered () {
+   document.getElementById("productimg").src = "../img/RedGuitar.jpg"
+}
+function togglepurple () {
+   document.getElementById("productimg").src = "../img/PurpleGuitar.jpg"
+}
+function toggleblack () {
+   document.getElementById("productimg").src = "../img/BlackGuitar.jpg"
+}
+function togglewhite () {
+   document.getElementById("productimg").src = "../img/WhiteGuitar.jpg"
+}
+
